2e21924b40911c8250115403e0387273479f5207
Joseph Jesus Florian Flores <jfloriaf@emeal.nttdata.com>
2022-03-06 22:45:14 -0500

    first commit

---
+++ src/main/java/mscertidigitaltitle/mscertidigitaltitle/MsCertidigitalTitleApplication.java
@@ -0,0 +1,13 @@
+package mscertidigitaltitle.mscertidigitaltitle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCertidigitalTitleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCertidigitalTitleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mscertidigitaltitle/mscertidigitaltitle/MsCertidigitalTitleApplicationTests.java
@@ -0,0 +1,13 @@
+package mscertidigitaltitle.mscertidigitaltitle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCertidigitalTitleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
